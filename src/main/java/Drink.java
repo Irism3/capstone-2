357ce76d5c84cc6bf5c8e945650c95dec34bc897
irismelendez <iris.13melendez@gmail.com>
2025-11-11 17:57:33 -0600

    Added Drink class extending to OrderItem class

--- src/main/java/Drink.java
+++ src/main/java/Drink.java
@@ -1,12 +1,37 @@
-public class Drink extends OrderItem{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Drink extends OrderItem {
 
     private String size;
     private String flavor;
 
-    public Drink(String displayName, String size, String flavor) {
-        super(displayName);
+    public Drink(String size, String flavor) {
+        super( "Drink");
         this.size = size;
         this.flavor = flavor;
+
+    }
+
+
+    @Override
+    public double getPrice() {
+        switch (size.toLowerCase()) {
+            case "small":
+                return 2.00;
+            case "medium":
+                return 2.50;
+            case "large":
+                return 3.00;
+            default:
+                return 0.0;
+
+        }
+    }
+
+    @Override
+    public String getSummary() {
+        return size + " Drink - " + flavor + "\n" + String.format("Price: $%.2f", getPrice());
     }
 
 }
